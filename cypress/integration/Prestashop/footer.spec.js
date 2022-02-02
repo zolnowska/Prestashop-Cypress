25316f227306c37791ceba25fe413f87a5571fb0
Sabina Zolnowska <[sabina.zolnowska@gmail.com]>
2022-02-02 22:34:43 +0100

    * setup eslint * setup prettier

--- cypress/integration/Prestashop/footer.spec.js
+++ cypress/integration/Prestashop/footer.spec.js
@@ -3,138 +3,135 @@
 
 /// const { should } = require("chai");
 
-describe("Footer: ", () => {
+describe('Footer: ', () => {
   before(() => {
-    cy.visit("/gb/");
-  });
+    cy.visit('/gb/')
+  })
 
   it(
     'User clicks button "New products" on the "Prices drop" subpage. ' +
       'After user clicks button "Sign in".',
     () => {
-      cy.visit("/gb/prices-drop");
+      cy.visit('/gb/prices-drop')
       cy.contains(
         '[id="link-product-page-new-products-1"]',
-        "New products"
-      ).click();
+        'New products'
+      ).click()
       cy.get('[id="js-product-list-header"]')
-        .should("be.visible")
-        .contains("New products");
-      cy.get("h4").should("be.visible").contains("No products available yet");
+        .should('be.visible')
+        .contains('New products')
+      cy.get('h4').should('be.visible').contains('No products available yet')
       cy.get('[id="content"] p')
-        .should("be.visible")
+        .should('be.visible')
         .contains(
-          "Stay tuned! More products will be shown here as they are added."
-        );
-      let searchCategoryInput = 1;
+          'Stay tuned! More products will be shown here as they are added.'
+        )
+      let searchCategoryInput = 1
       cy.get('[aria-label="Search"]')
         .eq(searchCategoryInput)
-        .should("be.visible")
-        .and("have.attr", "placeholder", "Search our catalog");
-      cy.contains('[title="Log in to your customer account"] span', "Sign in");
-      let clothesSubmenuBtn = 0;
+        .should('be.visible')
+        .and('have.attr', 'placeholder', 'Search our catalog')
+      cy.contains('[title="Log in to your customer account"] span', 'Sign in')
+      let clothesSubmenuBtn = 0
       cy.get('[class="category-sub-menu"] a')
         .eq(clothesSubmenuBtn)
-        .should("be.visible")
-        .and("have.text", "Clothes");
-      cy.url().should("include", "new-products");
-      cy.contains('[id="_desktop_user_info"] span', "Sign in").click();
-      cy.get('[type="password"]').should("be.visible");
-      cy.url().should("include", "login").and("include", "my-account");
+        .should('be.visible')
+        .and('have.text', 'Clothes')
+      cy.url().should('include', 'new-products')
+      cy.contains('[id="_desktop_user_info"] span', 'Sign in').click()
+      cy.get('[type="password"]').should('be.visible')
+      cy.url().should('include', 'login').and('include', 'my-account')
     }
-  );
+  )
 
   it(
     'User clicks button "Best sales" on the "New products" subpage. ' +
       'After user clicks button "Contact us".',
     () => {
-      cy.visit("/gb/new-products");
-      cy.contains(
-        '[id="link-product-page-best-sales-1"]',
-        "Best sales"
-      ).click();
+      cy.visit('/gb/new-products')
+      cy.contains('[id="link-product-page-best-sales-1"]', 'Best sales').click()
       cy.get('[id="js-product-list-header"]')
-        .should("be.visible")
-        .contains("Best sellers");
+        .should('be.visible')
+        .contains('Best sellers')
       cy.get('[id="wrapper"] span')
-        .should("be.visible")
-        .contains("Best sellers");
+        .should('be.visible')
+        .contains('Best sellers')
       cy.get('[id="content"] h4')
-        .should("be.visible")
-        .contains("No products available yet");
+        .should('be.visible')
+        .contains('No products available yet')
       cy.get('[type="email"]')
-        .should("be.visible")
-        .and("have.attr", "placeholder", "Your email address");
-      cy.url().should("include", "best-sales");
-      cy.contains('[id="contact-link"] a', "Contact us").click();
+        .should('be.visible')
+        .and('have.attr', 'placeholder', 'Your email address')
+      cy.url().should('include', 'best-sales')
+      cy.contains('[id="contact-link"] a', 'Contact us').click()
       cy.get('[id="contactform-message"] ')
-        .should("be.visible")
-        .and("have.attr", "placeholder", "How can we help?");
-      cy.url().should("include", "contact-us");
+        .should('be.visible')
+        .and('have.attr', 'placeholder', 'How can we help?')
+      cy.url().should('include', 'contact-us')
     }
-  );
+  )
 
   it(
     'User clicks button "Delivery" on the "Login in" subpage. ' +
       'After user clicks button "Art".',
     () => {
-      cy.visit("/gb/login?back=my-account");
-      cy.contains('[id="link-cms-page-1-2"]', "Delivery").click();
-      cy.get('[id="main"] h1').should("be.visible").contains("Delivery");
+      cy.visit('/gb/login?back=my-account')
+      cy.contains('[id="link-cms-page-1-2"]', 'Delivery').click()
+      cy.get('[id="main"] h1').should('be.visible').contains('Delivery')
       cy.get('[id="content"] h2')
-        .should("be.visible")
-        .contains("Shipments and returns");
+        .should('be.visible')
+        .contains('Shipments and returns')
       cy.get('[id="content"] h3')
-        .should("be.visible")
-        .contains("Your pack shipment");
-      cy.url().should("include", "delivery");
-      cy.contains('[id="category-9"] a', "Art").click();
-      cy.url().should("include", "art");
+        .should('be.visible')
+        .contains('Your pack shipment')
+      cy.url().should('include', 'delivery')
+      cy.contains('[id="category-9"] a', 'Art').click()
+      cy.url().should('include', 'art')
     }
-  );
+  )
 
   it(
     'User clicks button "Legal Notice" on the "Create an account" subpage. ' +
       'After user clicks button "Accessories".',
     () => {
-      cy.visit("/gb/login?create_account=1");
-      cy.contains('[id="link-cms-page-2-2"]', "Legal Notice").click();
+      cy.visit('/gb/login?create_account=1')
+      cy.contains('[id="link-cms-page-2-2"]', 'Legal Notice').click()
       cy.get('[id="content-wrapper"] h1')
-        .should("be.visible")
-        .contains("Legal Notice");
-      cy.get('[id="content"] h2').should("be.visible").contains("Legal");
-      cy.get('[id="content"] h3').should("be.visible").contains("Credits");
-      cy.url().should("include", "legal-notice");
-      cy.contains('[id="category-6"] a', "Accessories").click();
-      cy.url().should("include", "accessories");
+        .should('be.visible')
+        .contains('Legal Notice')
+      cy.get('[id="content"] h2').should('be.visible').contains('Legal')
+      cy.get('[id="content"] h3').should('be.visible').contains('Credits')
+      cy.url().should('include', 'legal-notice')
+      cy.contains('[id="category-6"] a', 'Accessories').click()
+      cy.url().should('include', 'accessories')
     }
-  );
+  )
 
   it(
     'User clicks button "Terms and conditions of use" on the "Acessories" subpage. ' +
       'After user clicks button "Home Accessories".',
     () => {
-      cy.visit("/gb/6-accessories");
+      cy.visit('/gb/6-accessories')
       cy.contains(
         '[id="link-cms-page-3-2"]',
-        "Terms and conditions of use"
-      ).click();
+        'Terms and conditions of use'
+      ).click()
       cy.get('[id="main"] h1')
-        .should("be.visible")
-        .contains("Terms and conditions of use");
+        .should('be.visible')
+        .contains('Terms and conditions of use')
       cy.get('[id="content"] h1')
-        .should("be.visible")
-        .contains("Terms and conditions of use");
-      cy.url().should("include", "terms-and-conditions-of-use");
+        .should('be.visible')
+        .contains('Terms and conditions of use')
+      cy.url().should('include', 'terms-and-conditions-of-use')
       cy.get('[id="category-6"] a')
-        .should("be.visible")
-        .and("contain", "Accessories")
-        .realHover();
+        .should('be.visible')
+        .and('contain', 'Accessories')
+        .realHover()
       cy.get('[id="category-8"] a')
-        .should("contain", "Home Accessories")
-        .and("be.visible")
-        .realClick();
-      cy.url().should("include", "home-accessories");
+        .should('contain', 'Home Accessories')
+        .and('be.visible')
+        .realClick()
+      cy.url().should('include', 'home-accessories')
     }
-  );
-});
+  )
+})
--- cypress/integration/Prestashop/homepage.spec.js
+++ cypress/integration/Prestashop/homepage.spec.js
@@ -13,114 +13,107 @@ import {
   loginIn,
   men,
   allProducts,
-  searchResult,
-} from "../../support/selectors.js";
+  searchResult
+} from '../../support/selectors.js'
 
-describe("Homepage: ", () => {
+describe('Homepage: ', () => {
   beforeEach(() => {
-    cy.visit("/gb/");
-  });
+    cy.visit('/gb/')
+  })
 
   it('User clicks button "Contact Us"', () => {
-    cy.contains(topMenu.contactUsBtn, "Contact us").click();
-    cy.get(contactUs.messageInput).as("messageInput");
-    cy.get("@messageInput")
-      .invoke("attr", "placeholder")
-      .should("contain", "How can we help?");
-    cy.get("@messageInput").click();
-    cy.url().should("include", "contact-us");
-  });
+    cy.contains(topMenu.contactUsBtn, 'Contact us').click()
+    cy.get(contactUs.messageInput).as('messageInput')
+    cy.get('@messageInput')
+      .invoke('attr', 'placeholder')
+      .should('contain', 'How can we help?')
+    cy.get('@messageInput').click()
+    cy.url().should('include', 'contact-us')
+  })
 
   it(
-    "User changes language of the website to Polish and after changes back to English GB " +
+    'User changes language of the website to Polish and after changes back to English GB ' +
       'by using drop down displayed after clicks button "English GB"',
     () => {
       //English GB is default language, on drop down is displayed Polski and English GB
       // user change language to Polski
-      cy.url().should("include", "gb");
-      cy.get(topMenu.languageBtn).as("languageBtn");
-      cy.get("@languageBtn").click();
-      cy.get(topMenu.secondLanguageDropDown).as("secondLanguageDropDown");
-      cy.get("@secondLanguageDropDown")
-        .should("be.visible")
-        .and("have.text", "English GB");
-      cy.get(topMenu.firstLanguageDropDown).as("firstLanguageDropDown");
-      cy.get("@firstLanguageDropDown").and("have.text", "Polski").click();
-      cy.url().should("include", "pl");
+      cy.url().should('include', 'gb')
+      cy.get(topMenu.languageBtn).as('languageBtn')
+      cy.get('@languageBtn').click()
+      cy.get(topMenu.secondLanguageDropDown).as('secondLanguageDropDown')
+      cy.get('@secondLanguageDropDown')
+        .should('be.visible')
+        .and('have.text', 'English GB')
+      cy.get(topMenu.firstLanguageDropDown).as('firstLanguageDropDown')
+      cy.get('@firstLanguageDropDown').and('have.text', 'Polski').click()
+      cy.url().should('include', 'pl')
       //user changes back lanugage to English GB
-      cy.get("@languageBtn").should("have.text", "Polski").click();
-      cy.get("@secondLanguageDropDown").and("have.text", "English GB").click();
-      cy.url().should("include", "gb");
+      cy.get('@languageBtn').should('have.text', 'Polski').click()
+      cy.get('@secondLanguageDropDown').and('have.text', 'English GB').click()
+      cy.url().should('include', 'gb')
     }
-  );
+  )
 
   it('User clicks button "Sign in".', () => {
-    cy.contains(topMenu.signInBtn, "Sign in").click();
-    cy.contains(
-      loginIn.createAccountBtn,
-      "No account? Create one here"
-    ).click();
-    cy.url().should("include", "create_account");
-  });
+    cy.contains(topMenu.signInBtn, 'Sign in').click()
+    cy.contains(loginIn.createAccountBtn, 'No account? Create one here').click()
+    cy.url().should('include', 'create_account')
+  })
 
   it('Button "Basket(0)" is displayed. Its inactive and doesnt have any product inside.', () => {
-    cy.get(topMenu.basketBtn)
-      .should("be.visible")
-      .and("have.class", "inactive");
-    cy.get(topMenu.basketText).should("be.visible").contains("Basket");
-    cy.get(topMenu.basketproductsCountText)
-      .should("be.visible")
-      .contains("(0)");
-  });
+    cy.get(topMenu.basketBtn).should('be.visible').and('have.class', 'inactive')
+    cy.get(topMenu.basketText).should('be.visible').contains('Basket')
+    cy.get(topMenu.basketproductsCountText).should('be.visible').contains('(0)')
+  })
 
   it('User mouse-over button "CLOTHES" in top menu and clicks "MEN" button from the drop down.', () => {
-    cy.get(navigation.clothesBtn).realHover();
-    cy.get(navigation.womenBtn).should("be.visible").contains("Women");
-    cy.get(navigation.menBtn).should("be.visible").contains("Men").realClick();
-    cy.get(men.menText).should("be.visible").contains("Men");
-  });
+    cy.get(navigation.clothesBtn).realHover()
+    cy.get(navigation.womenBtn).should('be.visible').contains('Women')
+    cy.get(navigation.menBtn).should('be.visible').contains('Men').realClick()
+    cy.get(men.menText).should('be.visible').contains('Men')
+  })
 
   //This test is failing because function "Search our catalog" doesn't work for Prestashop 1.7.8.2
   //I skip it for now and I will search more information why Preshashop doesn't display any results
   it.skip('User searches "sweater" in inputfield "Search our catalog".', () => {
     cy.get(navigation.searchInput)
-      .should("have.attr", "placeholder", "Search our catalog")
-      .type("sweater{enter}");
+      .should('have.attr', 'placeholder', 'Search our catalog')
+      .type('sweater{enter}')
     cy.get(searchResult.hummingbridPrintedSweaterText)
-      .should("be.visible")
-      .contains("Hummingbird printed sweater");
+      .should('be.visible')
+      .contains('Hummingbird printed sweater')
     cy.get(searchResult.regularPriceText)
-      .should("be.visible")
-      .contains("£43.08");
-    cy.get(searchResult.priceText).should("be.visible").contains("£34.46");
-    cy.url().should("include", "search").and("include", "sweater");
-  });
+      .should('be.visible')
+      .contains('£43.08')
+    cy.get(searchResult.priceText).should('be.visible').contains('£34.46')
+    cy.url().should('include', 'search').and('include', 'sweater')
+  })
 
-  it("User clicks to check next and previous image on this banner.", () => {
-    cy.get(banner.section).as("section").should("be.visible");
+  it('User clicks to check next and previous image on this banner.', () => {
+    cy.get(banner.section).as('section').should('be.visible')
     cy.get(banner.titleText)
-      .as("titleText")
-      .should("be.visible")
-      .contains("Sample 1");
-    cy.get(banner.previousBtn).click();
-    cy.get("@section").should("be.visible");
-    cy.get("@titleText").should("be.visible").contains("Sample 3");
-    cy.get(banner.nextBtn).click();
-    cy.get("@section").should("be.visible");
-    cy.get("@titleText").should("be.visible").contains("Sample 1");
-  });
+      .as('titleText')
+      .should('be.visible')
+      .contains('Sample 1')
+    cy.get(banner.previousBtn).click()
+    cy.get('@section').should('be.visible')
+    cy.get('@titleText').should('be.visible').contains('Sample 3')
+    cy.get(banner.nextBtn).click()
+    cy.get('@section').should('be.visible')
+    cy.get('@titleText').should('be.visible').contains('Sample 1')
+  })
 
   it('User clicks the button "All products".', () => {
-    cy.contains(homepage.allProductsBtn, "All products").click();
-    cy.get(allProducts.homeText).should("be.visible").contains("Home");
-    cy.url().should("include", "home");
-  });
+    cy.contains(homepage.allProductsBtn, 'All products').click()
+    cy.get(allProducts.homeText).should('be.visible').contains('Home')
+    cy.url().should('include', 'home')
+  })
 
-  it("User tries subscribe newsletter by already used e-mail", () => {
-    cy.get(newsletter.emailInput).type("test@test.com");
-    cy.get(newsletter.subscribeBtn).click();
+  it('User tries subscribe newsletter by already used e-mail', () => {
+    cy.get(newsletter.emailInput).type('test@test.com')
+    cy.get(newsletter.subscribeBtn).click()
     cy.get(newsletter.alertText)
-      .should("be.visible")
-      .contains("This email address is already registered.");
-  });
-});
+      .should('be.visible')
+      .contains('This email address is already registered.')
+  })
+})
